05f6d9fbb452f9113d10aa5f12deeaf00cb54dfe
Thai Le <thailekha@yahoo.com>
2020-02-10 17:42:38 -0600

    serve frontend

---
+++ build/precache-manifest.bd9a7dba37d76182f45a28d03581da52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcb79c24523ec2d47b8c32cc4be6ac47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4fbbe6e475e5e89cf441",
+    "url": "/static/css/main.89f91186.chunk.css"
+  },
+  {
+    "revision": "59ce5078a550af453f19",
+    "url": "/static/js/2.beb04a3a.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.beb04a3a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4fbbe6e475e5e89cf441",
+    "url": "/static/js/main.361f1d6d.chunk.js"
+  },
+  {
+    "revision": "c67e7fc494555f284ce1",
+    "url": "/static/js/runtime-main.a43b6eb2.js"
+  }
+]);
